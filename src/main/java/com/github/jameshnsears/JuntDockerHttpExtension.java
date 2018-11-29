db3a4e9dce805f01db0377eb389db53e1b68c58d
James Sears <james.hn.sears@gmail.com>
2018-11-29 12:42:16 +0000

    wip

--- src/main/java/com/github/jameshnsears/JuntDockerHttpExtension.java
+++ src/main/java/com/github/jameshnsears/JuntDockerHttpExtension.java
@@ -1,20 +1,40 @@
 package com.github.jameshnsears;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
+import java.util.Properties;
 
 public class JuntDockerHttpExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
 
     private static final Logger logger = LoggerFactory.getLogger(JuntDockerHttpExtension.class.getName());
 
+    private void readProps() {
+        Properties prop = new Properties();
+        try (InputStream inputStream = JuntDockerHttpExtension.class.getResourceAsStream("/junit-docker-http.properties")) {
+
+            prop.load(inputStream);
+            logger.info( prop.getProperty("a", "default"));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(System.out);
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
     @Override
     public void beforeTestExecution(ExtensionContext context) throws Exception {
         Method testMethod = context.getRequiredTestMethod();
+        readProps();
         logger.info(String.format("%s", testMethod.getName()));
     }
 
@@ -22,6 +42,5 @@ public class JuntDockerHttpExtension implements BeforeTestExecutionCallback, Aft
     public void afterTestExecution(ExtensionContext context) throws Exception {
         Method testMethod = context.getRequiredTestMethod();
         logger.info(String.format("%s", testMethod.getName()));
-
     }
 }
--- src/test/java/com/github/jameshnsears/JunitDockerHttpExtensionTest.java
+++ src/test/java/com/github/jameshnsears/JunitDockerHttpExtensionTest.java
@@ -3,12 +3,15 @@ package com.github.jameshnsears;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @ExtendWith(JuntDockerHttpExtension.class)
 public class JunitDockerHttpExtensionTest {
 
-    @Test
-    void testExtension() {
+    @ParameterizedTest
+    @ValueSource(strings = {"Hello"})
+    void testExtension(String message) {
         Assertions.assertEquals(1, 1);
     }
 }
