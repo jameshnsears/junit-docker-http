d8526dfe7d4fcd4ff4ad0235a7e0bfa76a70034b
James Sears <james.hn.sears@gmail.com>
2018-12-03 09:42:40 +0000

    wip

--- src/test/java/com/github/jameshnsears/DockerHttpTest.java
+++ src/test/java/com/github/jameshnsears/DockerHttpTest.java
@@ -3,42 +3,29 @@ package com.github.jameshnsears;
 import com.github.jameshnsears.model.Container;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import io.swagger.client.ApiException;
-import io.swagger.client.api.ContainerApi;
-import io.swagger.client.api.ImageApi;
-import io.swagger.client.model.ContainerSummary;
-import io.swagger.client.model.ImageSummary;
 import org.junit.Assert;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.util.Collection;
-import java.util.List;
 
 public class DockerHttpTest {
-    private static final Logger log = LoggerFactory.getLogger(DockerHttp.class);
-
     @Test
-    public void lsContainers() throws Exception {
-        // TODO: test validations
-        DockerHttp dockerHttp = new DockerHttp();
+    public void lsContainersGson() throws Exception {
+//        DockerHttp dockerHttp = new DockerHttp();
 //        String jsonResponse = dockerHttp.lsContainers();
-
-        Gson gson = new Gson();
 //        Container container = gson.fromJson(jsonResponse, Container.class);
 //        Container container = gson.fromJson(new InputStreamReader(is), Container.class);
 
+        Gson gson = new Gson();
         InputStream is = getClass().getResourceAsStream("/fixtures/lsContainers.json");
-
-        Type collectionType = new TypeToken<Collection<Container>>(){}.getType();
+        Type collectionType = new TypeToken<Collection<Container>>() {
+        }.getType();
         Collection<Container> containers = gson.fromJson(new InputStreamReader(is), collectionType);
-
-
-        Assert.assertEquals("", "");
+        Assert.assertEquals(containers.size(), 9);
+        Assert.assertEquals(((Container) ((java.util.ArrayList) containers).get(0)).getId(), "sha256:7466ebd20c18d0a9decfba4a12b0f8c4cce87e38866b512ffd6d483bfb4c24e1");
+        Assert.assertEquals(((java.util.ArrayList) ((Container) ((java.util.ArrayList) containers).get(0)).getRepoTags()).get(0), "jameshnsears/xqa-shard:latest");
     }
 }
